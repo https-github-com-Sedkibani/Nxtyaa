e8d2629e04a6d6db4742cb404a44ba45174d5eba
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-10 18:04:54 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -25,9 +25,9 @@ pipeline {
         
         stage('Build') {
             steps {
-                sh 'docker build -t banisedki/php1-fpm:latest -f ./ci/infrastructure/docker/php1-fpm/Dockerfile . '
+                sh 'docker build -t banisedki/php1-fpm:latest -f ./infrastructure/docker/php1-fpm/Dockerfile . '
                 
-                sh 'docker build -t banisedki/nxtya_nginx1:latest -f ./ci/infrastructure/docker/nginx1/Dockerfile . '
+                sh 'docker build -t banisedki/nxtya_nginx1:latest -f ./infrastructure/docker/nginx1/Dockerfile . '
 
 			 // 'docker build -t nxtya:1.0 -f docker/Dockerfile .'
             }
