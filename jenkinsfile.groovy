e861858ec03b60b72fce112f508fe22f6e68e84d
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-11 03:18:21 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,14 +4,14 @@ pipeline {
     stage ('prepare')
         { steps    {
                
-          sh 'rm -rf ./infrastructure'
+          sh 'rm -rf ./ci/infrastructure'
           sh ' rm -rf docker-compose.yml'
           sh 'cp -r /var/www/ci/infrastructure/ .'
           sh 'cp -r  /var/www/ci/infrastructure/docker/docker-compose.yml . '
           sh 'cp -r .env.example .env '  
             
              //prepare ansible-playbook 
-         sh 'ansible-playbook -i ./infrastructure/ansible/inventory/hosts.yml ./infrastructure/ansible/playbooks/install-docker.yml '
+         sh 'ansible-playbook -i ./ci/infrastructure/ansible/inventory/hosts.yml ./ci/infrastructure/ansible/playbooks/install-docker.yml '
         }
          }
    
@@ -25,11 +25,10 @@ pipeline {
         
         stage('Build') {
             steps {
-                sh 'docker build -t banisedki/php1-fpm:latest -f ./ci/infrastructure/docker/php1-fpm/Dockerfile . '
+                sh 'docker build -t banisedki/php1-fpm:latest -f ./infrastructure/docker/php1-fpm/Dockerfile . '
                 
-                sh 'docker build -t banisedki/nxtya1_nginx:latest -f ./ci/infrastructure/docker/nginx1/Dockerfile . '
+                sh 'docker build -t banisedki/nxtya1_nginx:latest -f ./infrastructure/docker/nginx1/Dockerfile . '
 
-			 // 'docker build -t nxtya:1.0 -f docker/Dockerfile .'
             }
         }
  
@@ -43,8 +42,8 @@ pipeline {
             }      
        /*stage('Push to Docker Hub') {
             steps {
-                            sh 'docker push banisedki/php-fpm:latest' 
-                            sh 'docker push banisedki/nxtya_nginx:latest'
+                            sh 'docker push banisedki/php1-fpm:latest' 
+                            sh 'docker push banisedki/nxtya1_nginx:latest'
                   }
                                    }*/
 
@@ -62,11 +61,7 @@ pipeline {
     		   sh 'docker exec php1-fpm php artisan view:clear'
      		   sh 'docker exec php1-fpm php artisan config:clear'	
         
-                // Use Ansible playbook to deploy to DigitalOcean server
-                //ansiblePlaybook(
-                   // playbook: '/var/lib/jenkins/workspace/nxtya/ansible.yml',
-                    //inventory: '/var/lib/jenkins/workspace/nxtya/inventory.ini',
-                    //extras: "-e 'docker_image=nxtya:1.0'"
+
                 
             }
         
