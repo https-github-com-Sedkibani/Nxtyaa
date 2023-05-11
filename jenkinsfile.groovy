091665d5dfc4d437656088a2bb92c6705df3fd43
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-11 02:35:29 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -35,15 +35,14 @@ pipeline {
 
         stage('Deploy') {
             steps {
-                sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
-		sh 'docker exec php-fpm rm -rf composer.lock vendor'
-		sh 'docker exec php-fpm composer require laravel/nova'
-		sh 'docker exec php-fpm composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
-		sh 'docker exec php-fpm chmod -R 0777 /var/www/html/storage'
-		sh 'docker exec php-fpm php artisan key:generate'
-		sh 'docker exec php-fpm php artisan config:cache'
-		sh 'docker exec php-fpm php artisan view:clear'
-		sh 'docker exec php-fpm php artisan config:clear'
+               sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
+               sh 'docker exec  php1-fpm rm -rf composer.lock vendor'
+               sh  'docker exec  php1-fpm composer install --ignore-platform-reqs --optimize-autoloader --prefer-dist --no-scripts -o --no-dev'
+               sh ' docker exec  php1-fpm chmod -R 0777 /var/www/html/storage'
+                sh 'docker exec  php1-fpm php artisan key:generate'
+                sh 'docker exec  php1-fpm php artisan config:cache'
+                sh 'docker exec  php1-fpm php artisan view:clear'
+                sh 'docker exec  php1-fpm php artisan config:clear'
               
                   }
                        }
