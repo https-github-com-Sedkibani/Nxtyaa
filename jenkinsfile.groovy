f76cc3d07b970b8ce72bc4470b3396bc18a47924
Sedkibani <112370909+Sedkibani@users.noreply.github.com>
2023-05-11 02:48:25 +0100

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,21 +4,30 @@ pipeline {
     stage ('prepare')
         { steps    {
                
-          sh 'rm -rf ./ci/infrastructure'
+          sh 'rm -rf ./infrastructure'
           sh ' rm -rf docker-compose.yml'
-          sh 'cp -r /var/www/ci/infrastructure/ .'
-          sh 'cp -r  /var/www/ci/infrastructure/docker/docker-compose.yml . '
-          sh 'cp -r .env.example .env ' 
+          sh 'cp -r /var/www/infrastructure/ .'
+          sh 'cp -r  /var/www/infrastructure/docker/docker-compose.yml . '
+          sh 'cp -r .env.example .env '  
+            
              //prepare ansible-playbook 
          sh 'ansible-playbook -i ./infrastructure/ansible/inventory/hosts.yml ./infrastructure/ansible/playbooks/install-docker.yml '
         }
          }
    
+ 
+    
+       /* stage('Checkout') {
+            steps {  
+                git branch: 'main', credentialsId: 'SedkiBani', url: 'git@github.com:https-github-com-Sedkibani/app_laravel.git'
+            }
+        }*/
+        
         stage('Build') {
             steps {
-                sh 'docker build -t banisedki/php1-fpm:latest -f ./infrastructure/docker/php1-fpm/Dockerfile . '
+                sh 'docker build -t banisedki/php-fpm:latest -f ./infrastructure/docker/php-fpm/Dockerfile . '
                 
-                sh 'docker build -t banisedki/nxtya_nginx1:latest -f ./infrastructure/docker/nginx1/Dockerfile . '
+                sh 'docker build -t banisedki/nxtya_nginx:latest -f ./infrastructure/docker/nginx/Dockerfile . '
 
 			 // 'docker build -t nxtya:1.0 -f docker/Dockerfile .'
             }
@@ -30,14 +39,21 @@ pipeline {
                // some block
                sh "docker login -u banisedki -p ${dockerHubPwd2}"
                   }
-                               }           
-                  }      
+            }           
+            }      
+       /*stage('Push to Docker Hub') {
+            steps {
+                            sh 'docker push banisedki/php-fpm:latest' 
+                            sh 'docker push banisedki/nxtya_nginx:latest'
+                  }
+                                   }*/
 
+
+     
         stage('Deploy') {
-          
-          
-   		 steps {
-       		 sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
+            steps {
+               
+              sh 'COMPOSE_HTTP_TIMEOUT=480 docker-compose up -d'
       		 sh 'docker exec php1-fpm rm -rf composer.lock vendor'
        		 sh 'docker exec php1-fpm apt-get update'
       		 sh 'docker exec php1-fpm apt-get install -y git'
@@ -47,15 +63,21 @@ pipeline {
        		 sh 'docker exec php1-fpm php artisan config:cache'
        		 sh 'docker exec php1-fpm php artisan view:clear'
        		 sh 'docker exec php1-fpm php artisan config:clear'
-   				 }
-			}
-
-              
-                  
-                       }
-	    
+        
+                // Use Ansible playbook to deploy to DigitalOcean server
+                //ansiblePlaybook(
+                   // playbook: '/var/lib/jenkins/workspace/nxtya/ansible.yml',
+                    //inventory: '/var/lib/jenkins/workspace/nxtya/inventory.ini',
+                    //extras: "-e 'docker_image=nxtya:1.0'"
+                
+            }
+        
+    }
        stage('Clean') {
             steps {
+               // Stop and remove old  docker container
+               // sh 'docker stop $(docker ps -a -q)'
+                //sh 'docker rm $(docker ps -a -q)'
           sh   'docker system prune -af --filter "until=24h" '
             }
         }
